6afbeff5be39ce77b1b9b19d21bf002b7dbe09a2
Issamoh <hm_ben_messaoud@esi.dz>
2023-10-19 01:23:09 +0100

    fixed connection to postgres database

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const db = require('./db'); // Import the pool from the db.js file
+//const db = require('./db'); // Import the pool from the db.js file
 require('dotenv').config();
 
 app.use(express.json());
 
 // Require and use your quote routes
 const quoteRoutes = require('./routes/quotes');
-app.use('/quotes', quoteRoutes);
+app.use('/api/v1/quotes', quoteRoutes);
 
 app.listen(port, () => {
   console.log(`Quotes API is listening on port ${port}`);
